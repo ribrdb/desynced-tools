7feb130eebe45b2e6b059340e33c86fcc488eb4e
Ryan Brown <ryan@derivita.com>
2023-12-27 08:08:30 -0700

    Fix paths in snapshots.

--- tests/compile.test.ts
+++ tests/compile.test.ts
@@ -2,13 +2,13 @@ import { expect, test } from "@jest/globals";
 import * as ts from "typescript";
 import { CompilerOptions, compileProgram } from "../compile";
 import { globSync } from "glob";
+import * as path from "path";
 
 for (const filename of globSync(`${__dirname}/*.ts`)) {
-  console.log(filename);
   if (filename.endsWith(".test.ts")) {
     continue;
   }
-  test(filename, () => {
+  test(path.basename(filename), () => {
     const files = [filename, `${__dirname}/../behavior.d.ts`];
     const program = ts.createProgram(files, {
       types: [],
