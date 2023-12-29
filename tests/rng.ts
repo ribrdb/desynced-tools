814043836334063409507d86dfcabb7fc5796b63
Benjamin Lerman <4838081+quisar@users.noreply.github.com>
2023-12-29 18:53:32 +0100

    Update test to not use num from Value

--- tests/rng.ts
+++ tests/rng.ts
@@ -1,9 +1,9 @@
 export function rng(max:Value, min:Value, state:Value) {
     unlock();
-    let r = max.num - min.num + 1;
-    if (state.num <= 0) {
+    let r = max - min + 1;
+    if (state <= 0) {
         state = factionItemAmount("metalore");
     }
-    state = modulo(17364*state.num+1, 65521);
-    return r*state.num/65521+min.num;
+    state = modulo(17364*state+1, 65521);
+    return r*state/65521+min;
 }
