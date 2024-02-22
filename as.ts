7714dca283e17bbc9898334c0a0371945217c9f8
swazrgb <65694696+swazrgb@users.noreply.github.com>
2024-02-23 00:17:06 +0100

    Upgrade to latest DesyncedJavaScriptUtils (#47)
    * Upgrade to latest DesyncedJavaScriptUtils
    
    Remove tods.ts and switch to serialization provided by DesyncedJavaScriptUtils
    
    Fixes #45
    
    Vendors https://github.com/StageGames/DesyncedJavaScriptUtils/commit/1a92339440d181fed0f43eebf78ef6266f57c2be
    
    * Remove unused functions
    
    Vendored from https://github.com/StageGames/DesyncedJavaScriptUtils/commit/492c9b70332400ccbfcaa1bc375661e153474afd
    
    * In encode, make sure integer object keys don't stay strings
    Also allow a gap of 1 in the array part of the encoded table
    
    * Make sure hashed numerical keys use 0-based index
    
    ---------
    
    Co-authored-by: b-stage <65645561+b-stage@users.noreply.github.com>

--- as.ts
+++ as.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as fs from "fs";
-import { ObjectToDesyncedString } from "./tods";
+import { ObjectToDesyncedString } from "./dsconvert";
 import { assemble } from "./assembler";
 
 const code = fs.readFileSync(process.argv[2], "utf8");
@@ -9,7 +9,8 @@ assemble(code).then((obj) => {
   if ('frame' in obj) {
     typ = 'B';
   }
-  ObjectToDesyncedString(obj, typ).then((str)=> fs.writeFileSync(process.argv[2] + ".txt", str));
+  const str = ObjectToDesyncedString(obj, typ);
+  fs.writeFileSync(process.argv[2] + ".txt", str);
   fs.writeFileSync(
     process.argv[2] + ".json",
     JSON.stringify(obj, undefined, 2)
